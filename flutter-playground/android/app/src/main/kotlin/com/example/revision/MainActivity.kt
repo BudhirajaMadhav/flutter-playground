a096c386d45630d7db000ee4dc411ed7d7d252d6
BudhirajaMadhav <madhavbudhiraja131@gmail.com>
2022-05-14 04:12:16 +0530

    renamed

--- flutter-playground/android/app/src/main/kotlin/com/example/revision/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.revision
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- flutter-playground/lib/main.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:revision/screens/BookMyShow.dart';
-
-void main() {
-  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: BookMyShow()));
-}
