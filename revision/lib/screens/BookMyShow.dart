f1845eac2c57e4b8a3ecf8b6c82d11d3680b792c
BudhirajaMadhav <madhavbudhiraja131@gmail.com>
2022-05-29 22:08:03 +0530

    made bms app

--- revision/lib/screens/BookMyShow.dart
+++ revision/lib/screens/BookMyShow.dart
@@ -13,10 +13,61 @@ class BookMyShow extends StatelessWidget {
           body: SingleChildScrollView(
               child: Column(
             children: [
-              Card(child: ShowCard()),
-              Card(child: ShowCard()),
+              Card(
+                child: ShowCard(),
+                elevation: 15,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10)),
+                margin: EdgeInsets.fromLTRB(10, 15, 10, 15),
+                shadowColor: Colors.grey.withOpacity(0.5),
+              ),
+              Card(
+                child: ShowCard(),
+                elevation: 15,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10)),
+                margin: EdgeInsets.fromLTRB(10, 15, 10, 15),
+                shadowColor: Colors.grey.withOpacity(0.5),
+              ),
+              Card(
+                child: ShowCard(),
+                elevation: 15,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10)),
+                margin: EdgeInsets.fromLTRB(10, 15, 10, 15),
+                shadowColor: Colors.grey.withOpacity(0.5),
+              ),
+              Card(
+                child: ShowCard(),
+                elevation: 15,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10)),
+                margin: EdgeInsets.fromLTRB(10, 15, 10, 15),
+                shadowColor: Colors.grey.withOpacity(0.5),
+              ),
             ],
-          ))),
+          )),
+          bottomNavigationBar: BottomNavigationBar(
+            type: BottomNavigationBarType.fixed,
+            items: [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label: "Home",
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.search),
+                label: "Search",
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.favorite_border_rounded),
+                label: "Just For You",
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.person),
+                label: "Profile",
+              ),
+            ],
+          )),
     );
   }
 }
--- revision/lib/widgets/showCard.dart
+++ revision/lib/widgets/showCard.dart
@@ -9,13 +9,7 @@ class ShowCard extends StatelessWidget {
         height: 300,
         decoration: BoxDecoration(
           color: Colors.white,
-          borderRadius: BorderRadius.circular(10),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.grey.withOpacity(0.5),
-              spreadRadius: 5,
-            ),
-          ],
+          borderRadius: BorderRadius.circular(5),
         ),
         child: Column(children: [
           Expanded(
@@ -23,8 +17,9 @@ class ShowCard extends StatelessWidget {
                 children: [
                   Image.network(
                     "https://i.imgur.com/RaFOxAU.jpg",
-                    width: double.infinity,
+                    // width: double.infinity,
                     fit: BoxFit.cover,
+                    height: double.infinity,
                   ),
                   Positioned(
                     bottom: 25,
@@ -46,7 +41,56 @@ class ShowCard extends StatelessWidget {
                                   TextStyle(color: Colors.white, fontSize: 15)),
                           backgroundColor: Colors.black.withOpacity(0.5),
                         )),
-                  )
+                  ),
+                  Material(
+                    elevation: 10,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Color(0xff59d367),
+                        boxShadow: [
+                          // shadow on right and bottom
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.5),
+                            spreadRadius: 0,
+                            offset: Offset(2, 2),
+                          ),
+                        ],
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.fromLTRB(5, 2, 5, 2),
+                        child:
+                            Text("New", style: TextStyle(color: Colors.white)),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                      right: 10,
+                      bottom: 15,
+                      child: Container(
+                          decoration: BoxDecoration(
+                            color: Colors.black.withOpacity(0.5),
+                            borderRadius: BorderRadius.circular(5),
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.all(4.0),
+                            child: Column(
+                              children: [
+                                Row(children: [
+                                  Icon(
+                                    Icons.favorite,
+                                    color: Colors.red,
+                                    size: 18,
+                                  ),
+                                  SizedBox(width: 5),
+                                  Text("87%",
+                                      style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 20)),
+                                ]),
+                                SizedBox(height: 5),
+                                Text("7,809 votes",
+                                    style: TextStyle(color: Colors.white))
+                              ],
+                            ),
+                          ))),
                 ],
               ),
               flex: 3),
@@ -54,18 +98,21 @@ class ShowCard extends StatelessWidget {
               child: Padding(
                 padding: const EdgeInsets.all(10.0),
                 child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
                           Text("Hindi Medium",
                               style: TextStyle(
-                                  fontWeight: FontWeight.bold, fontSize: 20)),
+                                  fontWeight: FontWeight.bold, fontSize: 15)),
+                          SizedBox(height: 3),
                           Row(
                             children: [
-                              Text("Hindi", style: TextStyle(fontSize: 15)),
+                              Text("Hindi", style: TextStyle(fontSize: 12)),
+                              SizedBox(width: 5),
                               Container(
                                 decoration: ShapeDecoration(
                                   shape: RoundedRectangleBorder(
@@ -73,28 +120,31 @@ class ShowCard extends StatelessWidget {
                                       color: Colors.grey.shade300,
                                       width: 1,
                                     ),
-                                    borderRadius: BorderRadius.circular(5),
+                                    borderRadius: BorderRadius.circular(3),
                                   ),
                                 ),
                                 child: Padding(
-                                  padding: const EdgeInsets.all(5.0),
-                                  child:
-                                      Text("2D", style: TextStyle(fontSize: 15)),
+                                  padding:
+                                      const EdgeInsets.fromLTRB(4, 1, 4, 1),
+                                  child: Text("2D",
+                                      style: TextStyle(fontSize: 13)),
                                 ),
                               ),
                             ],
                           )
                         ]),
                     Container(
+                      margin: EdgeInsets.only(right: 3),
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 16, vertical: 7),
                       decoration: BoxDecoration(
-                        color: Colors.red,
+                        color: Color(0xffff2e55),
                         borderRadius: BorderRadius.circular(5),
                       ),
-                      height: 40,
-                      width: 100,
-                      alignment: Alignment.center,
                       child: Text("BOOK",
-                          style: TextStyle(fontSize: 20, color: Colors.white)),
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold)),
                     ),
                   ],
                 ),
